601e5d874f77fb6974c1b6ff21b7ae52abe74ace
Jonathan Cowher <jrcowher@gmail.com>
2019-11-17 18:47:45 -0500

    Add blinking cursor to letters

---
+++ TODO.md
@@ -0,0 +1,6 @@
+## Welcome Banner
+- [x] Add blinking underscore cursor that follows text as it types in
+
+## Welcome Banner to Homepage transition
+- [ ] Welcome Banner and Homepage scroll left until Homepage is left justified
+- [ ] Homepage elements animate in (determine what this means)
--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-import $ from 'jquery';
-import 'foundation-sites';
-import 'what-input';
+(($, document) => {
 
-// Foundation JS relies on a global varaible. In ES6, all imports are hoisted
-// to the top of the file so if we used `import` to import Foundation, it would
-// execute earlier than we have assigned the global variable. This is why we
-// have to use CommonJS require() here since it doesn't have the hoisting
-// behavior.
-window.jQuery = $;
+  $(document).ready(() => {
+    $(document).foundation();
+    $('.section--home').find('h2').find('b').each(function (i) {
+      let $this = $(this);
 
-$(document).foundation();
+      setTimeout(function () {
+        $this.addClass('visible current').siblings('b').removeClass('current');
+      }, parseFloat($this.data('animationDelay')));
+    });
+  });
+
+})(jQuery, document);
