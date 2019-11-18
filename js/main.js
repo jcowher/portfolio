614b89d6d11619be452ad2f0a1c74bbb75effc37
Jonathan Cowher <jrcowher@gmail.com>
2019-11-17 19:14:37 -0500

    Slide home section in after welcome banner displays

--- js/main.js
+++ js/main.js
@@ -2,13 +2,18 @@
 
   $(document).ready(() => {
     $(document).foundation();
-    $('.section--home').find('h2').find('b').each(function (i) {
+
+    $('.section--welcome').find('h2').find('b').each(function (i) {
       let $this = $(this);
 
-      setTimeout(function () {
+      setTimeout(() => {
         $this.addClass('visible current').siblings('b').removeClass('current');
       }, parseFloat($this.data('animationDelay')));
     });
+
+    setTimeout(() => {
+      $('.section--home, .section--welcome').addClass('did-welcome');
+    }, 10250);
   });
 
 })(jQuery, document);
